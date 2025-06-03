4e812ded91e21fadcf4f436840cf3b6bbb712410
Luis Cipriani <cipriani.luisguilherme@gmail.com>
2025-06-03 11:59:27 -0300

    Cria cabeçalho

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
 
-export default function Header(){
+import "../Header/header.css";
 
-    return(
-        <div>
-            <h1>Cabeçalho</h1>
+export default function Header() {
+
+    const [pesquisa, setPesquisa] = useState("");
+
+    return (
+        <div className="headerContainer">
+            <div className="aboveHeader">
+                <text className="textAbove">SOS Unipet: <b>0800 000 0000</b></text>
+                <text className="textAbove2">Orientação em Saúde 24 horas: <b>0800 000 0000</b></text>
+                <Link className="linkAbove">Mais telefones úteis</Link>
+                <div className="searchContainer">
+                    <input
+                        className="pesquisa"
+                        type="text"
+                        value={pesquisa}
+                        placeholder="O que procura?"
+                        onChange={(e) => setPesquisa(e.target.value)}
+                    />
+                    <FaSearch className="searchIcon" />
+                </div>
+            </div>
+            <div className="belowHeader">
+                <h2>texto</h2>
+            </div>
         </div>
     )
 }
